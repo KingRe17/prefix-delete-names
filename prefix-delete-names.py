ebfc230113a043b297f38599eab049cee57a7ed8
KingRe17 <reynaldo17r@yahoo.com.br>
2025-07-04 14:07:43 -0300

    Add files via upload

---
+++ prefix-delete-names.py
@@ -0,0 +1,22 @@
+﻿import glob
+import os
+
+local = input('local = ')
+
+tudo = glob.glob(local + '\\*')
+print(tudo)
+
+qtd = int(input('delete how many characters ? '))
+
+for i in range(len(tudo)):
+
+ arqu = os.path.basename(tudo[i])  # receive 1 file from the list (only name without path)
+ 
+ f = arqu[qtd:]
+ 
+ fim = local + '\\' + f
+
+ os.rename(tudo[i], fim)
+
+
+
